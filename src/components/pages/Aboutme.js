2769af9ee3a960340229f9daec42543703b0c01d
Tyler Mifflin <you@example.com>
2023-07-19 13:44:00 -0600

    added a paragraph about me

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -4,3 +4,11 @@ export default function Aboutme() {
     return (
         <div>
             <h1>About Me</h1>
+            <p>
+            My name is Tyler Mifflin, I am a creative web developer. I have a positive outlook on life.
+            I am passionate about physical and mental health, outdoor recreation, the environment, and photography.
+            I enjoy snowboarding, surfing, hiking, traveling, and spending time in the mountains.
+            </p>
+        </div>
+    )
+}
