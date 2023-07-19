154940f4ccc6ea9df40bc4d9e75ec0599de7b363
Tyler Mifflin <you@example.com>
2023-07-19 14:00:28 -0600

    set up contact form, since we don't have a backend yeti didn't do the submit form since it wouldn't work

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Contact() {
+    return (
+        <div>
+            <h1>Contact Me</h1>
+            <p>
+            If you have any questions, please contact me at tyler.mifflin1@hotmail.com or reach me at 801-580-6702.
+            </p>
+        </div>
+    )
+}
+
