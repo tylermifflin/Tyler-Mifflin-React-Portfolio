61fd9c6c1b1dc8ad259f56f3396a85fec2379b0c
Tyler Mifflin <you@example.com>
2023-07-23 16:26:16 -0700

    got the nav bar set up in the header to use hrefs for each page of my portfolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,35 @@
 import React from 'react';
 
 export default function Header({currentpage, handlepagechange ]) {
-  
+    return (
+        <header>
+            <h1>Tyler Mifflin</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#aboutme" onClick={() => handlepagechange('Aboutme')} className={currentpage === 'Aboutme' ? 'nav-link active' : 'nav-link'}>
+                            About Me
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#portfolio" onClick={() => handlepagechange('Portfolio')} className={currentpage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
+                            Portfolio
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#resume" onClick={() => handlepagechange('Resume')} className={currentpage === 'Resume' ? 'nav-link active' : 'nav-link'}>
+                            Resume
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#contact" onClick={() => handlepagechange('Contact')} className={currentpage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
 
