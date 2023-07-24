3d43911ecd80824d5d4e8b9b90fd258d52d4d3e3
Tyler Mifflin <you@example.com>
2023-07-23 17:19:46 -0700

    got rid of extra li tag in Resume.js

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -8,7 +8,6 @@ export default function Resume() {
             Here is a link to my resume:
             </p>
             <ul>
-                <li>
                     <a href="https://docs.google.com/document/d/17geriMyVB5jSeXVxP7kVoFsI4QCCEr3K/edit?usp=sharing&ouid=114561379459160840512&rtpof=true&sd=true">Tyler Mifflin's Resume</a>
             </ul>
         </div>
