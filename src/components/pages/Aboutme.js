8b6919f5af77769877b3366e45cbae424be3df6f
Tyler Mifflin <you@example.com>
2023-07-23 17:05:22 -0700

    added a profile pic in my aboutme page

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -8,6 +8,8 @@ export default function Aboutme() {
             My name is Tyler Mifflin, I am a creative web developer. I have a positive outlook on life.
             I am passionate about coding, physical and mental health, outdoor recreation, the environment, and photography.
             </p> 
+            {/* add about me picture here */}
+            <img src="../images/aboutme.jpg" alt="about me" />
         </div>
     )
 }
