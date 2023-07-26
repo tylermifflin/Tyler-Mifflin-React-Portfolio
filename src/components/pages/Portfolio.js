0909f94a1c7ec5c027f513faf141151f4b6a101c
Tyler Mifflin <you@example.com>
2023-07-26 17:56:36 -0600

    changed all classname to className

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -13,44 +13,74 @@ export default function Portfolio() {
             Here is a list of my projects:
             </p>
             {/* add 6 projects with an image, link to github and a link to deployed project */}
-            <div classname='row'>
-              <div classname='col-lg-4 col-md-6 md-4'>
-                <div classname='card'>
+            <div className='row'>
+              <div className='col-lg-4 col-md-6 mb-4'>
+                <div className='card'>
                  <img src="../images/priestlakenorthernlights.jpg" alt="the northern lights over Priest Lake" className="card-img-top" style={imagestyle} />
-                    <div classname='card-body'>
+                    <div className='card-body'>
                     <a href="https://becomethebestyou.herokuapp.com/">Become</a>
                     <a href="https://github.com/tylermifflin/Become">Github</a>
                     </div>
                 </div>
               <div>
-            <img src="../images/mesaarch.jpg" alt="Mesa Arch, Canyonlands" style={imagestyle} />
+            </div>
+            <div className='row'>
+              <div className='col-lg-4 col-md-6 md-4'>
+                <div className='card'>
+                 <img src="../images/mesaarch.jpg" alt="Mesa Arch, Canyonlands" style={imagestyle} />
+                    <div className='card-body'>
                     <a href="https://dkargis.github.io/team-Inception/" class="whats-that-plant">Sherpa Trails</a>
                     <a href="https://github.com/Dkargis/team-Inception">Github</a>
+                    </div>
+                </div>
+              <div>
             </div>
-            <div>
-                <img src="../images/northernlightsiceland21.jpg" alt="the northern lights in Iceland" style={imagestyle} />
-                <a href="https://tylermifflin.github.io/Weather-Tracker/" class="photography-project">Weather Tracker</a>
-                <a href="https://github.com/tylermifflin/Weather-Tracker">Github</a>
+            <div className='row'>
+                <div className='col-lg-4 col-md-6 md-4'>
+                    <div className='card'>
+                     <img src="../images/northernlightsiceland21.jpg" alt="the northern lights in Iceland" style={imagestyle} />
+                      <div className='card-body'>
+                      <a href="https://tylermifflin.github.io/Weather-Tracker/" class="photography-project">Weather Tracker</a>
+                      <a href="https://github.com/tylermifflin/Weather-Tracker">Github</a>
+                      </div>
+                    </div>
+                <div>
             </div>
-            <div>
-            <img src="../images/reflectioncanyon.jpeg" alt="reflection canyon" style={imagestyle} />
+            <div className='row'>
+                <div className='col-lg-4 col-md-6 md-4'>
+                 <div className='card'>
+                  <img src="../images/reflectioncanyon.jpeg" alt="reflection canyon" style={imagestyle} />
+                    <div className='card-body'>
                     <a href="https://superscheduler.herokuapp.com/" class="create-the-life-of-your-dreams">Super Scheduler</a>
                     <a href="https://github.com/tylermifflin/Super-Scheduler">Github</a>
+                    </div>
+                 </div>
+                <div>
             </div>
-            <div>
-            <img src="../images/montrotuiridge.jpg" alt="a mountain ridge overlooking the ocean" style={imagestyle} />
+            <div className='row'>
+             <div className='col-lg-4 col-md-6 md-4'>
+              <div className='card'>
+                <img src="../images/montrotuiridge.jpg" alt="a mountain ridge overlooking the ocean" style={imagestyle} />
+                    <div className='card-body'>
                     <a href="https://tylermifflin.github.io/Perfect-Planner/" class="travel-stoked">Perfect Planner</a>
                     <a href="https://github.com/tylermifflin/Perfect-Planner">Github</a>
+                    </div>
+              </div>
+             <div>
             </div>
-            <div>
+            <div className='row'>
+             <div className='col-lg-4 col-md-6 md-4'>
+              <div className='card'>
             <img src="../images/thewave.jpg" alt="the Wave, Arizona" style={imagestyle} />
+                    <div className='card-body'>
                     <a href="https://drive.google.com/file/d/17rxuHfYTDYamc2heJeu3Zq0QBeksTQWD/view" class="travel-stoked">Rapid ReadMe Producer</a>
                     <a href="https://github.com/tylermifflin/Rapid-README-Producer">Github</a>
+                    </div>
+                </div>
+                <div>
             </div>
-
-
         </div>
+    </div>
     )
 }
-
                 
