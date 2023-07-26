f97eb0792c75d0ab815d86d6ce4ea2afcd636045
Tyler Mifflin <you@example.com>
2023-07-26 17:38:38 -0600

    followed the error messages in react to properly use the image style for my images, changed height to 100% and using bootstrap to make a card and container, going to repeat on other 5 images

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,45 +1,49 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const imagestyle = {
-    height: "50vh",
-    display: "flex",
+    height: "100%",
 }
 
 export default function Portfolio() {
     return (
-        <div>
+        <div className='container'>
             <h1>Portfolio</h1>
             <p>
             Here is a list of my projects:
             </p>
             {/* add 6 projects with an image, link to github and a link to deployed project */}
-            <div>
-            <img src="../images/priestlakenorthernlights.jpg" alt="the northern lights over Priest Lake">{imagestyle}</img>
+            <div classname='row'>
+              <div classname='col-lg-4 col-md-6 md-4'>
+                <div classname='card'>
+                 <img src="../images/priestlakenorthernlights.jpg" alt="the northern lights over Priest Lake" className="card-img-top" style={imagestyle} />
+                    <div classname='card-body'>
                     <a href="https://becomethebestyou.herokuapp.com/">Become</a>
                     <a href="https://github.com/tylermifflin/Become">Github</a>
-            </div>
-            <div>
-            <img src="../images/mesaarch.jpg" alt="Mesa Arch, Canyonlands">{imagestyle}</img>
+                    </div>
+                </div>
+              <div>
+            <img src="../images/mesaarch.jpg" alt="Mesa Arch, Canyonlands" style={imagestyle} />
                     <a href="https://dkargis.github.io/team-Inception/" class="whats-that-plant">Sherpa Trails</a>
                     <a href="https://github.com/Dkargis/team-Inception">Github</a>
             </div>
             <div>
-                <img src="../images/northernlightsiceland21.jpg" alt="the northern lights in Iceland">{imagestyle}</img>
+                <img src="../images/northernlightsiceland21.jpg" alt="the northern lights in Iceland" style={imagestyle} />
                 <a href="https://tylermifflin.github.io/Weather-Tracker/" class="photography-project">Weather Tracker</a>
                 <a href="https://github.com/tylermifflin/Weather-Tracker">Github</a>
             </div>
             <div>
-            <img src="../images/reflectioncanyon.jpeg" alt="reflection canyon">{imagestyle}</img>
+            <img src="../images/reflectioncanyon.jpeg" alt="reflection canyon" style={imagestyle} />
                     <a href="https://superscheduler.herokuapp.com/" class="create-the-life-of-your-dreams">Super Scheduler</a>
                     <a href="https://github.com/tylermifflin/Super-Scheduler">Github</a>
             </div>
             <div>
-            <img src="../images/montrotuiridge.jpg" alt="a mountain ridge overlooking the ocean">{imagestyle}</img>
+            <img src="../images/montrotuiridge.jpg" alt="a mountain ridge overlooking the ocean" style={imagestyle} />
                     <a href="https://tylermifflin.github.io/Perfect-Planner/" class="travel-stoked">Perfect Planner</a>
                     <a href="https://github.com/tylermifflin/Perfect-Planner">Github</a>
             </div>
             <div>
-            <img src="../images/thewave.jpg" alt="the Wave, Arizona">{imagestyle}</img>
+            <img src="../images/thewave.jpg" alt="the Wave, Arizona" style={imagestyle} />
                     <a href="https://drive.google.com/file/d/17rxuHfYTDYamc2heJeu3Zq0QBeksTQWD/view" class="travel-stoked">Rapid ReadMe Producer</a>
                     <a href="https://github.com/tylermifflin/Rapid-README-Producer">Github</a>
             </div>
