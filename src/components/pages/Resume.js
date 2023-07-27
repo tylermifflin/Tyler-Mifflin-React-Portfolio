69974cd35b25e5ef5d203ee7c706ea15807362b6
Tyler Mifflin <you@example.com>
2023-07-27 17:20:29 -0600

    got my Resume link content to be the same size as p tag by using a span tag and including the link inside the p tag, and used a linkcontainer custom style and a custom link style const

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const linkcontainer = {
+    display: "flex",
+    alignItems: "center",
+}
+
+const linkstyle = {
+    marginLeft: "10px",
+}
+
 export default function Resume() {
     return (
         <div className='container'>
             <h2>Resume</h2>
             <div className='row'>
-                <div className='col-md-6'>
+                <div className='col'>
             <p>
             Here is a link to my resume:
+            <span style={linkcontainer}>
+                    <a href="https://docs.google.com/document/d/17geriMyVB5jSeXVxP7kVoFsI4QCCEr3K/edit?usp=sharing&ouid=114561379459160840512&rtpof=true&sd=true" style={linkstyle} >Tyler Mifflin's Resume</a>
+            </span>
             </p>
             </div>
-            <div className='col-md-6'>
-            <ul>
-                    <a href="https://docs.google.com/document/d/17geriMyVB5jSeXVxP7kVoFsI4QCCEr3K/edit?usp=sharing&ouid=114561379459160840512&rtpof=true&sd=true">Tyler Mifflin's Resume</a>
-            </ul>
-        </div>
         </div>
         </div>
     )
