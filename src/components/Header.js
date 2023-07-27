9cdf309e474e539ad7b40cb8c413ff64672eaf8b
Tyler Mifflin <you@example.com>
2023-07-26 23:33:10 -0600

    made my h1 and nav hrefs on same line

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
-const linkstyle = {
+const headerstyle = {
+    fontSize: '3rem',
+    color: 'aquamarine',
+    fontfamily: 'Gill Sans, Gill Sans MT, Calibri, Trebuchet MS, sans-serif',
+    justifyContent: 'center',
+    padding: '10px',
+    margin: '10px',
+};
+
+const navstyle = {
     fontSize: '2rem',
-    color: 'white'
+    color: 'white',
+    fontfamily: 'Gill Sans, Gill Sans MT, Calibri, Trebuchet MS, sans-serif',
+    
+    
 };
 
 
@@ -12,26 +24,26 @@ export default function Header({currentpage, handlepagechange }) {
             <div className='container-fluid'>
              <div className='row'>
                <div className='col text-center'>
-            <h1>Tyler Mifflin</h1>
             <nav className='navbar navbar-expand-lg'>
+            <h1 style={headerstyle}>Tyler Mifflin</h1>
                 <ul className='navbar-nav'>
                     <li className='nav-item'>
-                        <a href="#aboutme" style={linkstyle} onClick={() => handlepagechange('Aboutme')} className={currentpage === 'Aboutme' ? 'nav-link active' : 'nav-link'}>
+                        <a href="#aboutme" style={navstyle} onClick={() => handlepagechange('Aboutme')} className={currentpage === 'Aboutme' ? 'nav-link active' : 'nav-link'}>
                             About Me
                         </a>
                     </li>
                     <li className='nav-item'>
-                        <a href="#portfolio" style={linkstyle} onClick={() => handlepagechange('Portfolio')} className={currentpage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
+                        <a href="#portfolio" style={navstyle} onClick={() => handlepagechange('Portfolio')} className={currentpage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
                             Portfolio
                         </a>
                     </li>
                     <li className='nav-item'>
-                        <a href="#resume" style={linkstyle} onClick={() => handlepagechange('Resume')} className={currentpage === 'Resume' ? 'nav-link active' : 'nav-link'}>
+                        <a href="#resume" style={navstyle} onClick={() => handlepagechange('Resume')} className={currentpage === 'Resume' ? 'nav-link active' : 'nav-link'}>
                             Resume
                         </a>
                     </li>
                     <li className='nav-item'>
-                        <a href="#contact" style={linkstyle} onClick={() => handlepagechange('Contact')} className={currentpage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+                        <a href="#contact" style={navstyle} onClick={() => handlepagechange('Contact')} className={currentpage === 'Contact' ? 'nav-link active' : 'nav-link'}>
                             Contact
                         </a>
                     </li>
