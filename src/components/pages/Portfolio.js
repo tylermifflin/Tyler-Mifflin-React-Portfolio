98b6c2fa3ec1da8c7be648d625f6e1f8d5ae516a
Tyler Mifflin <you@example.com>
2023-07-26 18:18:34 -0600

    added extra card body tags to separate my links to the project and links to github

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -16,64 +16,67 @@ export default function Portfolio() {
             <div className='row'>
               <div className='col-lg-4 col-md-6 mb-4'>
                 <div className='card'>
-                 <img src="../images/priestlakenorthernlights.jpg" alt="the northern lights over Priest Lake" className="card-img-top" style={imagestyle} />
+                {/* i want to access the images in my images folder and display them here */}
+                 <img src="./images/priestlakenorthernlights.jpg" alt="the northern lights over Priest Lake" className="card-img-top" style={imagestyle} />
                     <div className='card-body'>
                     <a href="https://becomethebestyou.herokuapp.com/">Become</a>
+                    </div>
+                    <div className='card-body'>
                     <a href="https://github.com/tylermifflin/Become">Github</a>
                     </div>
                 </div>
-              <div>
-            </div>
-            <div className='row'>
+              </div>
               <div className='col-lg-4 col-md-6 mb-4'>
                 <div className='card'>
                  <img src="../images/mesaarch.jpg" alt="Mesa Arch, Canyonlands" style={imagestyle} />
                     <div className='card-body'>
                     <a href="https://dkargis.github.io/team-Inception/" class="whats-that-plant">Sherpa Trails</a>
+                    </div>
+                    <div className='card-body'>
                     <a href="https://github.com/Dkargis/team-Inception">Github</a>
                     </div>
                 </div>
-              <div>
-            </div>
-            <div className='row'>
+              </div>
                 <div className='col-lg-4 col-md-6 mb-4'>
                     <div className='card'>
                      <img src="../images/northernlightsiceland21.jpg" alt="the northern lights in Iceland" style={imagestyle} />
                       <div className='card-body'>
                       <a href="https://tylermifflin.github.io/Weather-Tracker/" class="photography-project">Weather Tracker</a>
+                        </div>
+                        <div className='card-body'>
                       <a href="https://github.com/tylermifflin/Weather-Tracker">Github</a>
                       </div>
                     </div>
                 </div>
-            </div>
-            <div className='row'>
                 <div className='col-lg-4 col-md-6 mb-4'>
                  <div className='card'>
                   <img src="../images/reflectioncanyon.jpeg" alt="reflection canyon" style={imagestyle} />
                     <div className='card-body'>
                     <a href="https://superscheduler.herokuapp.com/" class="create-the-life-of-your-dreams">Super Scheduler</a>
+                    </div>
+                    <div className='card-body'>
                     <a href="https://github.com/tylermifflin/Super-Scheduler">Github</a>
                     </div>
                  </div>
                 </div>
-            </div>
-            <div className='row'>
              <div className='col-lg-4 col-md-6 mb-4'>
               <div className='card'>
                 <img src="../images/montrotuiridge.jpg" alt="a mountain ridge overlooking the ocean" style={imagestyle} />
                     <div className='card-body'>
                     <a href="https://tylermifflin.github.io/Perfect-Planner/" class="travel-stoked">Perfect Planner</a>
+                    </div>
+                    <div className='card-body'>
                     <a href="https://github.com/tylermifflin/Perfect-Planner">Github</a>
                     </div>
               </div>
              </div>
-            </div>
-            <div className='row'>
              <div className='col-lg-4 col-md-6 mb-4'>
               <div className='card'>
             <img src="../images/thewave.jpg" alt="the Wave, Arizona" style={imagestyle} />
                     <div className='card-body'>
                     <a href="https://drive.google.com/file/d/17rxuHfYTDYamc2heJeu3Zq0QBeksTQWD/view" class="travel-stoked">Rapid ReadMe Producer</a>
+                    </div>
+                    <div className='card-body'>
                     <a href="https://github.com/tylermifflin/Rapid-README-Producer">Github</a>
                     </div>
                 </div>
