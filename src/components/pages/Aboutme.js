82d15ee424c5835fc161ba0f89cffba084fc4d75
Tyler Mifflin <you@example.com>
2023-07-27 16:35:23 -0600

    set up columns with bootstrap to have my image be on the right side on my about me text on the left side and be responsive

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -3,21 +3,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import aboutme from '../images/aboutme.jpg';
 
 const aboutmeimgstyle = {
-    height: "100%",
+    height: "75%",
     display: "flex",
 }
 
 
 export default function Aboutme() {
     return (
-        <div>
+        <div className='container'>
             <h1>About Me</h1>
-            <p>
-            My name is Tyler Mifflin, I am a creative web developer. I have a positive outlook on life.
-            I am passionate about coding, physical and mental health, outdoor recreation, the environment, and photography.
-            </p> 
+            <div className='row'>
+             <div className='col-md-6'>
+              <p>
+               My name is Tyler Mifflin, I am a creative web developer. I have a positive outlook on life.
+               I am passionate about coding, physical and mental health, outdoor recreation, the environment, and photography.
+              </p> 
+             </div>
+            <div className='col-md-6'>
             {/* add about me picture here */}
-            <img src={aboutme} alt="about me" className='card-img-top' style={aboutmeimgstyle} />
+            <img src={aboutme} alt="about me" className='img-fluid' style={aboutmeimgstyle} />
+            </div>
+            </div>
         </div>
     )
 }
