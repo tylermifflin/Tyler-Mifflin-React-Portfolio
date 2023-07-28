33abb0f790783e6debbc290d25fff47f96b73c7e
Tyler Mifflin <you@example.com>
2023-07-27 22:00:19 -0600

    called handleSubmit and handleCursor leave for name and added classname form control

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -32,9 +32,9 @@ export default function Contact() {
             If you would like to work with me, please contact me at tyler.mifflin1@hotmail.com or reach me at 801-580-6702.
             </p>
             {/* add a contact form that lets user put in name, email, and message and validates email */}
-            <form>
-                <label for="name">Name:</label>
-                <input type="text" id="name" name="name" placeholder="Your name.." required></input>
+            <form onSubmit={handleSubmit}>
+                <label htmlfor="name">Name:</label>
+                <input type="text" id="name" name="name" className='form-control' placeholder="Your name.." required onCusorleave={handleCursorleave}></input>
                 <label for="email">Email:</label>
                 <input type="email" id="email" name="email" placeholder="Your email.." required></input>
                 <label for="message">Message:</label>
